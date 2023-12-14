632d92a1826f567022595ae37b3b53c7161bda3d
Matthew Leibowitz <mattleibow@live.com>
2023-12-14 21:39:53 +0200

    Add initial code

---
+++ PackagingMauiAssets/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PackagingMauiAssets;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
